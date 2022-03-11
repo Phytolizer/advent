11b7da21e509ccdf6b43320126f70e2a0a60ccb1
Kyle Coffey <me@phytolizer.dev>
2022-03-11 12:49:09 -0600

    rename variable

--- modules/advent/include/advent.h
+++ modules/advent/include/advent.h
@@ -7,4 +7,4 @@
 #include <stdio.h>
 
 typedef ADVENT_ERROR_RETURN(int64_t) try_int64_t;
-try_int64_t advent_getline(char** p_line, size_t* p_length, FILE* stream);
+try_int64_t advent_getline(char** p_line, size_t* p_capacity, FILE* stream);
--- modules/advent/source/advent.c
+++ modules/advent/source/advent.c
@@ -9,9 +9,9 @@
 
 #define LINE_CHUNK_SIZE 64
 
-try_int64_t advent_getline(char** p_line, size_t* p_length, FILE* stream) {
-    if (*p_line == NULL || *p_length == 0) {
-        *p_length = LINE_CHUNK_SIZE;
+try_int64_t advent_getline(char** p_line, size_t* p_capacity, FILE* stream) {
+    if (*p_line == NULL || *p_capacity == 0) {
+        *p_capacity = LINE_CHUNK_SIZE;
         *p_line = malloc(LINE_CHUNK_SIZE);
         if (*p_line == NULL) {
             return (try_int64_t){.error = ENOMEM};
@@ -20,7 +20,7 @@ try_int64_t advent_getline(char** p_line, size_t* p_length, FILE* stream) {
     size_t len = 0;
     while (true) {
         char* fgets_result =
-            fgets(*p_line + len, (int)(*p_length - len), stream);
+            fgets(*p_line + len, (int)(*p_capacity - len), stream);
         len += strlen(*p_line + len);
         if (fgets_result == NULL) {
             return (try_int64_t){.value = (int64_t)len};
@@ -28,8 +28,8 @@ try_int64_t advent_getline(char** p_line, size_t* p_length, FILE* stream) {
         if ((*p_line)[len - 1] == '\n') {
             return (try_int64_t){.value = (int64_t)len - INT64_C(1)};
         }
-        *p_length *= 2;
-        char* new_buffer = realloc(*p_line, *p_length);
+        *p_capacity *= 2;
+        char* new_buffer = realloc(*p_line, *p_capacity);
         if (new_buffer == NULL) {
             return (try_int64_t){.error = ENOMEM};
         }
